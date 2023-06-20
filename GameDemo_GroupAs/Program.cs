e3d4db442b6e1d901482d2b2d31f83974bf91eff
Rasmus <rasmus.hornling@gmail.com>
2023-06-20 15:18:15 +0200

    Added a bunch of rocks :)

--- GameDemo_GroupAs/Program.cs
+++ GameDemo_GroupAs/Program.cs
@@ -2,9 +2,27 @@
 {
     internal class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ChooseYourFighter();
+        }
+
+        private static void ChooseYourFighter()
+        {
+            ConsoleKey input = Console.ReadKey().Key;
+            if (input == ConsoleKey.R) 
+            {
+                Console.WriteLine("rock");
+            }
+            else if (input == ConsoleKey.P) 
+            {
+                Console.WriteLine("paper");
+            }
+            else if ( input == ConsoleKey.S)
+            {
+                Console.WriteLine("scissors");
+            }
         }
     }
 }
