91c514efb21f53a0410053478e2d28243c2ce0e9
Tan Thai <Vi.Tan.Thai@gmail.com>
2023-06-20 14:56:54 +0200

    Added Tan-Branch

--- GameDemo_GroupAs/Program.cs
+++ GameDemo_GroupAs/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Rock - Paper - Scissors Game");
+            Console.WriteLine("1. Rock\n2. Paper\n 3.Scissors");
+
+            Console.WriteLine("Select your option");
+            int userInput = int.Parse(Console.ReadLine());
+
+            switch (userInput)
+            {
+                case 1: Console.WriteLine();
+                        break;
+                case 2: Console.WriteLine();
+                        break;
+                case 3: Console.WriteLine();
+                        break;
+                
+            }
         }
     }
 }
